d68e96bb1053f8b8a2993f91fd4e7df79ea35ae3
Stephen Guo <epicsteveoj@gmail.com>
2024-03-17 22:47:31 -0400

    Add icons

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,42 +1,46 @@
 <script lang="ts">
-
+	import GithubLogo from 'virtual:icons/bi/github';
+	import LinkedInLogo from 'virtual:icons/bi/linkedin';
+	import EnvelopeLogo from 'virtual:icons/bi/envelope-fill';
+	import FileTextLogo from 'virtual:icons/bi/file-earmark-text-fill';
 </script>
 
 <!-- bg-white  -->
 <footer class="flex flex-col items-center py-6 bg-[#feffee]">
-  <h2 class="text-black text-4xl pb-1">Contact me!</h2>
-  <div class="flex flex-row">
-    <a
-      class="text-lg flex items-center px-3 text-[#aa7c97] hover:text-[#633f5c]"
-      href="https://github.com/Epicsteve2"
-      target="_blank"
-    >
-      <!-- <Icon class="pr-1 w-fit" name="bi:github" /> -->
-      GitHub
-    </a>
-    <a
-      class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
-      href="https://www.linkedin.com/in/stephen-guo-399959192/"
-      target="_blank"
-    >
-      <!-- <Icon class="pr-1 w-fit" name="bi:linkedin" /> -->
-      LinkedIn
-    </a>
-    <a
-      class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
-      href="mailto:steve.guo@mail.utoronto.ca"
-      target="_blank"
-    >
-      <!-- <Icon class="pr-1 w-fit" name="bi:envelope-fill" /> -->
-      Email
-    </a>
-    <a
-      class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
-      href="https://github.com/Epicsteve2/LaTeX-Files/raw/main/pdf/StephenGuo_Resume.pdf"
-      target="_blank"
-    >
-      <!-- <Icon class="pr-1 w-fit" name="bi:file-earmark-text-fill" /> -->
-      Resume
-    </a>
-  </div>
+	<h2 class="text-black text-4xl pb-1">Contact me!</h2>
+	<div class="flex flex-row">
+		<a
+			class="text-lg flex items-center px-3 text-[#aa7c97] hover:text-[#633f5c]"
+			href="https://github.com/Epicsteve2"
+			target="_blank"
+		>
+			<!-- no "w-fill" -->
+			<GithubLogo class="pr-1" />
+			GitHub
+		</a>
+		<a
+			class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
+			href="https://www.linkedin.com/in/stephen-guo-399959192/"
+			target="_blank"
+		>
+			<LinkedInLogo class="pr-1" />
+			LinkedIn
+		</a>
+		<a
+			class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
+			href="mailto:steve.guo@mail.utoronto.ca"
+			target="_blank"
+		>
+			<EnvelopeLogo class="pr-1" />
+			Email
+		</a>
+		<a
+			class="text-lg flex items-center px-3 text-blue-600 hover:text-blue-800"
+			href="https://github.com/Epicsteve2/LaTeX-Files/raw/main/pdf/StephenGuo_Resume.pdf"
+			target="_blank"
+		>
+			<FileTextLogo class="pr-1" />
+			Resume
+		</a>
+	</div>
 </footer>
