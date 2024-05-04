c2dc6787d039d6652eb9c9a552cf337c735dfcee
Stephen Guo <epicsteveoj@gmail.com>
2024-05-04 16:31:12 -0400

    Fix build to not remove git worktree files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
+	build: {
+		// So git worktree would work with
+		emptyOutDir: false,
+	}
 	plugins: [
 		sveltekit(),
 		Icons({
